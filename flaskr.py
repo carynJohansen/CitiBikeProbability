666d75d7f360fbe1fcbb658d320209c10d8469c4
Colin VanLang <colinvanlang@gmail.com>
2015-06-20 14:18:29 -0400

    tutorial site up and running

--- flaskr.py
+++ flaskr.py
@@ -28,7 +28,7 @@ def before_request():
   g.db = connect_db()
 
 @app.teardown_request
-def teardown_request():
+def teardown_request(exception):
   db = getattr(g, 'db', None)
   if db is not None:
     db.close()
